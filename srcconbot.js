4cbf80d8a289906e5870dafc62b9b1776ef62277
Ryan Pitts <ryan.a.pitts@gmail.com>
2016-07-26 15:56:10 -0700

    try it without description graf

--- srcconbot.js
+++ srcconbot.js
@@ -72,7 +72,7 @@ var sendAlert = function(timeblock, message) {
                 'color': '#F79797',
                 'title': transcript.title,
                 'title_link': 'http://srccon.aloft.nu/2016-'+transcript.id,
-                'text': transcript.description,
+                //'text': transcript.description,
                 'fields': [
                     {
                         'title': 'Facilitator(s)',
@@ -326,7 +326,7 @@ var transcripts = {
             "title": "Keeping people at the forefront of data stories"
         }
     ],
-    [key('2016-07-26 15:48')]: [
+    [key('2016-07-26 15:58')]: [
         {
             "day": "Friday", 
             "description": "Science Fiction authors often embed deep insights into the future of technology within their stories. Come to this session to share examples of fascinating science fictional treatments of media and networked communication with other attendees and geek out about who got it right and who may yet come out correct. (My idea is to solicit ahead of time 4-6 super fans who are willing to give low-key lightning talks summarizing plots with an emphasis on the interesting media bits.)", 
@@ -358,7 +358,7 @@ var transcripts = {
             "title": "Give and Receive: Can we strengthen our community through remote mentorship and office hours?"
         }
     ],
-    [key('2016-07-26 15:49')]: [
+    [key('2016-07-26 15:59')]: [
         {
             "day": "Friday", 
             "description": "**This session would be great for you if you're comfortable working collaboratively (sketching and sharing) in small groups.**\n\nWriting down processes, goals, and workflows is an important part of building healthy, transparent, and collaborative teams. But finding time to write and making sure that people read those documents is a constant challenge. This activity-based brainstorm session will lean into the expertise and experience of attendees to explore methods for building solid documentation practices into a team's culture.", 
